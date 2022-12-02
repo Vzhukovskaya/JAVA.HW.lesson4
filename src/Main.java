382873baf2d50bcb933d64e2815ec2090323348b
user <valeriazhukovskaya789@gmail.com>
2022-12-02 20:00:27 +0200

    HW.lesson4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задача 1. Работа со String.
+        String strOne;
+        String strTwo;
+        strOne = " Vlad!";
+        strTwo = " How are u?";
+        System.out.println(strOne.length());
+        System.out.println(strOne.concat(strTwo)); //Канкатинация строк.
+
+        //Задача 2. Продемонстировать явное и неявное приведение типов.
+        int a = 7;
+        short b = 17;
+
+
+        b = (short) a; //Явное приведение.
+        a = b; //Неявное приведение.
+        System.out.println(a);
+        System.out.println(b);
+
+        //Задача 3. Показать переполнение.
+        byte b = 120;
+        b += 19;
+        System.out.println(b);
+
+        int l = (int)b; // Пробую byte привести к int - не понимаю почему не полчаеться.
+        System.out.println(l);
+
+        //Задача 4. Показать работу с методами и классами.
+        int m = 7;
+        int n = 170;
+
+        Second.Process();
+        Second.ProcessWithArg(m, n);
+        System.out.println(Second.Function());
+        System.out.println(Second.FunctionWithArg(m, n));
+        System.out.println(Second.FunctionStr());
+        System.out.println(Second.FunctionStrWithArg(strOne, strTwo));
+
     }
 }
--- src/Second.java
+++ src/Second.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Second {
+public class Second {
+    public static void Process() { //процедура без аргументов.
+        System.out.println(1 + 1);
+    }
+
+
+    public static void ProcessWithArg(int a, int b) {//процедура с аргументами.
+        System.out.println(a + b);
+    }
+
+
+    public static int Function() {  //функция int без арнументов.
+        return 1 + 1;
+    }
+
+
+    public static int FunctionWithArg(int a, int b) {//функция int с аргументами.
+        return a + b;
+    }
+
+
+    public static String FunctionStr() { //функция string без аргументов.
+        return "Hello Everyone!";
+    }
+
+
+    public static String FunctionStrWithArg(String a, String b) {//функция string с аргументами.
+        return ("Hello" + a + b);
+    }
 }
